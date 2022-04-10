e111a44aeeaaf016463e61b6f612f6763a31bed5
Callum Kirkman <56700517+CallumKirkman@users.noreply.github.com>
2022-04-10 15:28:24 +0100

    Create contact add screen

--- app/screens/ContactAddScreen.js
+++ app/screens/ContactAddScreen.js
@@ -1,23 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   StyleSheet,
   Text,
+  TextInput,
   View,
-  SafeAreaView,
+  KeyboardAvoidingView,
   TouchableOpacity,
 } from "react-native";
 
+import { auth } from "../components/firebase";
+import { addContact } from "../components/firestore";
 import Colour from "../static/Colour";
 
 const ContactAdd = ({ navigation }) => {
+  let email = auth.currentUser?.email;
+
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState("");
+
+  const runAddContact = () => {
+    // console.log(name);
+    // console.log(number);
+    addContact(email, name, number);
+    navigation.navigate("Contact");
+  };
+
   return (
-    <SafeAreaView
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <KeyboardAvoidingView style={styles.container} behavior="padding">
       <TouchableOpacity
         style={[styles.buttonBox]}
         onPress={() => navigation.navigate("Contact")}
@@ -26,7 +35,28 @@ const ContactAdd = ({ navigation }) => {
       </TouchableOpacity>
 
       <Text>ContactAdd screen</Text>
-    </SafeAreaView>
+
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Contact Name"
+          value={name}
+          onChangeText={setName}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Contact Number"
+          value={number}
+          onChangeText={setNumber}
+        />
+      </View>
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={[styles.button]} onPress={runAddContact}>
+          <Text style={styles.buttonText}>Add contact</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
   );
 };
 
@@ -35,7 +65,7 @@ export default ContactAdd;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: Colour.lightGray,
     alignItems: "center",
     justifyContent: "center",
   },
@@ -52,5 +82,25 @@ const styles = StyleSheet.create({
     color: Colour.white,
     fontWeight: "700",
     fontSize: 16,
+  },
+  inputContainer: { width: "80%" },
+  input: {
+    backgroundColor: Colour.white,
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    margin: 5,
+  },
+  buttonContainer: {
+    width: "60%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    backgroundColor: Colour.blue,
+    width: "100%",
+    padding: 15,
+    borderRadius: 10,
+    margin: 5,
   },
 });
